2facd6210ea75d15a7db4c3bac7c47d00d4ca64d
Musk <yuiop1029@naver.com>
2025-06-21 22:14:42 +0900

    fix: Add dummy test files to all modules (Navigation, Core, Domain, App) to resolve Tuist CI errors

---
+++ Projects/App/UITests/BuyingMyHomeUITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+
+final class BuyingMyHomeUITests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a UI test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let app = XCUIApplication()
+        app.launch()
+        XCTAssertTrue(true)
+    }
+} 
